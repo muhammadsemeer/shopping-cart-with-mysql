737d6f3231abfb5b54b7e476359fc330ffc80ad4
muhammadsemeer <muhammadsemeerali@gmail.com>
2020-10-20 13:41:36 +0530

    small changes to db connection and my orders page

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
+require("dotenv").config();
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "shopping",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
 });
 
 module.exports = connection;
